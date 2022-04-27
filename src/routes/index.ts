8c8cd4b6faa72ba451b2dc3a9a9671b51fdf1377
Calvin <calvin@edu.ink>
2022-04-27 15:26:40 +0800

    fix: fastify input/output validators

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import MongooseService from "@/services/MongooseService";
 import { FastifyPluginAsync } from "fastify";
+import { readFileSync } from "fs";
+import yaml from "js-yaml";
 import Months from "./handlers/Months";
 import Transactions from "./handlers/Transactions";
 
@@ -7,23 +9,23 @@ const main: FastifyPluginAsync = async (server) => {
   /** DB Models */
   await MongooseService.importModels(["Transaction", "Month"]);
 
-  // /** API Schema */
-  // const apiSchema = yaml.load(readFileSync(require.resolve("@/openapi/core.yaml"), "utf-8"));
-  // apiSchema.$id = "core";
-  // server.addSchema(apiSchema);
+  /** API Schema */
+  const apiSchema = yaml.load(readFileSync(require.resolve("@/openapi/docs.yaml"), "utf-8"));
+  Object.assign(apiSchema, { $id: "docs" });
+  server.addSchema(apiSchema);
 
   /** Routes */
   server.get("/", async function () { return { status: "running" }; });
 
   /** Transactions */
-  server.get(`/transactions`, Transactions.query);
-  server.post(`/transactions`, Transactions.insert);
-  server.get(`/transactions/:id`, Transactions.detail);
-  server.patch(`/transactions/:id`, Transactions.update);
+  server.get(`/transactions`, { schema: { response: { "2xx": { type: "array", items: { $ref: "docs#/components/schemas/Transaction" } } } } }, Transactions.query);
+  server.post(`/transactions`, { schema: { body: { $ref: "docs#/components/schemas/Transaction__edit" } } }, Transactions.insert);
+  server.get(`/transactions/:id`, { schema: { response: { "2xx": { $ref: "docs#/components/schemas/Transaction" } } } }, Transactions.detail);
+  server.patch(`/transactions/:id`, { schema: { body: { $ref: "docs#/components/schemas/Transaction__edit" } } }, Transactions.update);
   server.delete(`/transactions/:id`, Transactions.remove);
 
   /** Months */
-  server.get(`/months/:yearMonth`, Months.getByYearMonth);
+  server.get(`/months/:yearMonth`, { schema: { response: { "2xx": { $ref: "docs#/components/schemas/Month" } } } }, Months.getByYearMonth);
 };
 
 export default main;
